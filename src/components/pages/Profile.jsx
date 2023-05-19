c6c43e04f4f30d3665fe2b774028913113b2b634
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-05-19 17:28:20 +0200

    Attempting to fix deploy bug by renaming component

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -4,7 +4,7 @@ import {
   ProfileBookings,
   ProfileMeta,
   ProfileVenueBookings,
-  ProfileVenues,
+  RenderProfileVenues,
 } from '../profileData';
 
 const profileUrl = 'https://api.noroff.dev/api/v1/holidaze';
@@ -149,7 +149,7 @@ export default function Profile({ setFilteredVenues }) {
         />
 
         {profile.venueManager && profileVenues && profileVenues.length > 0 && (
-          <ProfileVenues
+          <RenderProfileVenues
             key={venueUpdates}
             venues={profileVenues}
             handleBookingsSlideIn={handleBookingsSlideIn}
