289ec079370b007bbd523d37cfb17a83c52399eb
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-05-23 17:20:55 +0200

    Created component to handle searchModal

---
+++ src/components/UI/UI_components/Searching.jsx
@@ -0,0 +1,47 @@
+import { Box, IconButton, styled } from '@mui/joy';
+import SearchIcon from '@mui/icons-material/Search';
+import { SearchVenueModal } from '../../modals';
+import { useState } from 'react';
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  color:
+    theme.palette.mode === 'dark'
+      ? theme.palette.primary[700]
+      : theme.palette.neutral[400],
+  borderRadius: '100vw',
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? theme.palette.neutral[50]
+      : theme.palette.primary[500],
+  border:
+    theme.palette.mode === 'dark'
+      ? `4px solid ${theme.palette.primary[700]}`
+      : `4px solid ${theme.palette.neutral[400]}`,
+
+  ':hover': {
+    backgroundColor:
+      theme.palette.mode === 'dark'
+        ? theme.palette.neutral[300]
+        : theme.palette.primary[700],
+  },
+}));
+
+export default function Searching({ venues }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box>
+      <StyledIconButton
+        size='lg'
+        aria-label='Search venues'
+        onClick={handleOpen}>
+        <SearchIcon />
+      </StyledIconButton>
+
+      <SearchVenueModal venues={venues} open={open} handleClose={handleClose} />
+    </Box>
+  );
+}
